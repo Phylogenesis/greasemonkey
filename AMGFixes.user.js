2868c6150d1b831f960374531e2b25bf6cd7dd3a
Phylogenesis <jonesld@gmail.com>
2015-05-11 15:27:28 +0100

    Create AMGFixes.user.js
    Added GreaseMonkey script to fix minor issues with A Mining Game

---
+++ AMGFixes.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         A Mining Game: Fix minor issues
+// @namespace    https://github.com/Phylogenesis/greasemonkey/
+// @version      0.1
+// @description  Fixes some minor issues with the chat system and allows manual saving by clicking on the button in the top right
+// @author       Luke Jones
+// @match        http://trugul.com/index.php
+// @grant        none
+// ==/UserScript==
+
+(function ($) {
+    var AGame = {
+        client: undefined
+    };
+    
+    $(document).on(
+        'click',
+        'div.tabs a',
+        function () {
+            setTimeout(
+                function () {
+                    if ($('div.chat-room-message-container:visible').length > 0) {
+                        $('div.chat-room-message-container').scrollTop($('div.chat-room-message-container')[0].scrollHeight);
+                        $('body').css('margin-bottom', $('div.popout').outerHeight() + 'px');
+                    } else {
+                        $('body').css('margin-bottom', 0);
+                    }
+                },
+                100
+            );
+        }
+    );
+
+    $(document).on(
+        'click',
+        'ul.navbar-right a',
+        function (e) {
+            if (AGame.client) {
+                AGame.client.emit('save');
+            }
+            
+            e.preventDefault();
+        }
+    );
+    
+    setTimeout(function () {
+        AGame.client = io('http://5.189.137.117:469');
+    }, 5000);
+})(jQuery);
+    
