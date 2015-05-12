1ec848ffc4ab8dd51833456b56dcbbd7d1d0dcb8
Phylogenesis <jonesld@gmail.com>
2015-05-12 16:56:33 +0100

    Update AMGFixes.user.js
    Added a current leaderboard ranking box to the top-left corner.

--- AMGFixes.user.js
+++ AMGFixes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         A Mining Game: Fix minor issues
 // @namespace    https://github.com/Phylogenesis/
-// @version      0.1.5
+// @version      0.2
 // @description  Fixes some minor issues with the chat system and allows manual saving by clicking on the button in the top right
 // @author       Luke Jones
 // @include      /^http://trugul\.com/(index\.php)?$/
@@ -11,7 +11,41 @@
 
 (function ($) {
     var AGame = {
-        client: undefined
+        client:     undefined,
+        username:   undefined,
+        lastSubmit: undefined,
+    };
+    
+    function updateRanks() {
+        $.ajax({
+            url:     'http://trugul.com/highscores/',
+            async:   true,
+            success: function (data) {
+                var matches = /You are ranked #(\d+)/.exec(data);
+                var rank = 'N/A'
+                
+                if (matches.length > 0) {
+                    rank = matches[1];
+                }
+                
+                $('#leaderboardRanking span.mainRank').text(rank);
+            }
+        });
+        
+        $.ajax({
+            url:     'http://trugul.com/highscores/?order=prestige',
+            async:   true,
+            success: function (data) {
+                var matches = /You are ranked #(\d+)/.exec(data);
+                var rank = 'N/A'
+                
+                if (matches.length > 0) {
+                    rank = matches[1];
+                }
+                
+                $('#leaderboardRanking span.prestigeRank').text(rank);
+            }
+        });
     };
     
     $(document).on(
@@ -45,7 +79,32 @@
     );
     
     setTimeout(function () {
-        AGame.client = io('http://5.189.137.117:469');
+        AGame.client   = io('http://5.189.137.117:469');
+        AGame.username = $('#playerName').text();
+            
+        AGame.client.on(
+            'gameVars',
+            function (data) {
+                var lastSubmit = data.gameVars.lastSubmit;
+                
+                if (AGame.lastSubmit !== lastSubmit) {
+                    AGame.lastSubmit = lastSubmit;
+                    updateRanks();
+                }
+            }
+        );
+        
+        $('#quickdetails tr').prepend('<td id="leaderboardRanking" class="displaytext2" style="text-align: center; width: 70px;"><b>Ranking</b><br><span class="mainRank">&hellip;</span> | <span class="prestigeRank">&hellip;</span></td>');
+
+        $('span.mainRank, span.prestigeRank').css('cursor', 'pointer');
+
+        $('span.mainRank').click(function () {
+            window.open('http://trugul.com/highscores/?user=' + AGame.username)
+        });
+
+        $('span.prestigeRank').click(function () {
+            window.open('http://trugul.com/highscores/?order=prestige&user=' + AGame.username)
+        });
     }, 5000);
 })(jQuery);
     
